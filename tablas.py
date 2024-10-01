f15f3cba446520d4ccde6fa50f81d369c62c8d4a
Diana-Camargo <dianacamargo2006@yahoo.com>
2024-10-01 00:32:35 -0600

    Add files via upload

---
+++ tablas.py
@@ -0,0 +1,81 @@
+#materias más usadas
+grado1="1ro de primaria"
+materia1="Matemáticas"
+materia2="Lengua materna"
+materia3="Educación física"
+grado2="2do de primaria"
+materia4="Lengua materna"
+materia5="Matemáticas"
+materia6="Formación cívica y ética"
+grado3="3ro de primaria"
+materia7="Español"
+materia8="Ciencias naturales"
+materia9="Inglés"
+grado4="4to de primaria"
+materia10="Español"
+materia11="Historia"
+materia12="Matemáticas"
+grado5="5to de primaria"
+materia13="Historia"
+materia14="Geografía"
+materia15="Formación cívica y ética"
+print("{:^20} {:^20} {:^20} {:^20}".format("Grado","Materia 1","Materia 2","Materia 3"))
+print("{:^20} {:^20} {:^20} {:^20}".format(grado1, materia1,  materia2,  materia3))
+print("{:^20} {:^20} {:^20} {:^20}".format(grado2, materia4,  materia5,  materia6))
+print("{:^20} {:^20} {:^20} {:^20}".format(grado3, materia7,  materia8,  materia9))
+print("{:^20} {:^20} {:^20} {:^20}".format(grado4, materia10, materia11, materia12))
+print("{:^20} {:^20} {:^20} {:^20}".format(grado5, materia13, materia14, materia15))
+#primeros lugares en la primaria
+grado1="1ro de primaria"
+lugar1="Karla"
+lugar2="Luis"
+lugar3="Ana"
+grado2="2do de primaria"
+lugar4="Carlos"
+lugar5="Carmen"
+lugar6="José"
+grado3="3ro de primaria"
+lugar7="Armando"
+lugar8="Sofía"
+lugar9="Oscar"
+grado4="4to de primaria"
+lugar10="Valeria"
+lugar11="Brenda"
+lugar12="Verónica"
+grado5="5to de primaria"
+lugar13="William"
+lugar14="Francisco"
+lugar15="Luisa"
+print("{:<20} {:<20} {:<20} {:<20}".format("Grado","primer lugar","segundo lugar","tercer lugar"))
+print("{:<20} {:<20} {:<20} {:<20}".format(grado1,lugar1,lugar2,lugar3))
+print("{:<20} {:<20} {:<20} {:<20}".format(grado2,lugar4,lugar5,lugar6))
+print("{:<20} {:<20} {:<20} {:<20}".format(grado3,lugar7,lugar8,lugar9))
+print("{:<20} {:<20} {:<20} {:<20}".format(grado4,lugar10,lugar11,lugar12))
+print("{:<20} {:<20} {:<20} {:<20}".format(grado5,lugar13,lugar14,lugar15))
+#sabritas favoritas
+niño1="Aldahir"
+s1="ruffles"
+s2="takis"
+s3="adobadas"
+niño2="Jordan"
+s4="runners"
+s5="doritos"
+s6="tostitos"
+niño3="Rossy"
+s7="fritos"
+s8="chips"
+s9="chetos"
+niño4="Karina"
+s10="pake taxo"
+s11="churrumais"
+s12="crujitos"
+niño5="Ulises"
+s13="rancheritos"
+s14="pringles"
+s15="conchitas"
+print("{:>20} {:>20} {:>20} {:>20}".format("niños","sabrita1","sabrita2","sabrita3"))
+print("{:>20} {:>20} {:>20} {:>20}".format(niño1,s1,s2,s3))
+print("{:>20} {:>20} {:>20} {:>20}".format(niño2,s4,s5,s6))
+print("{:>20} {:>20} {:>20} {:>20}".format(niño3,s7,s8,s9))
+print("{:>20} {:>20} {:>20} {:>20}".format(niño4,s10,s11,s12))
+print("{:>20} {:>20} {:>20} {:>20}".format(niño5,s13,s14,s15))
